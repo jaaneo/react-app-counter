ed7d5b531a2b8abc29b2a65587586223091d00ca
Alejandro <jaaneo@gmail.com>
2023-03-02 20:20:34 -0300

    Change the text from  spanish to english

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,15 @@ function App() {
     setCount(count-1)
   }
   
-  return (
-   
-
-    <div class="bg-gradient-to-r from-sky-500 to-indigo-500 p-6">
-
+  return (  
+    <div class="bg-gradient-to-r from-sky-500 to-indigo-500 p-4">
             <h1 class="max-w-3xl mb-4 text-5xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">¡Challenge number two! </h1>
-            <h2 class="text-4xl font-bold dark:text-white">Current counter: {count}</h2>
-          
-           <p class="m-3">
-            <ButtonIncrement text="Aumentar" onClick={handleIncrement} />
-            <ButtonDecrement text="Disminuir" onClick={handleDecrement} />
-           </p>
-          
-    </div>
-
-
-  
+            <h2 class="text-4xl font-bold dark:text-white">Current counter: {count}</h2>         
+           <p class="p-4">
+            <ButtonIncrement text="Increment" onClick={handleIncrement} />
+            <ButtonDecrement text="Decrement" onClick={handleDecrement} />
+           </p>        
+    </div>  
   );
 }
-
 export default App;
