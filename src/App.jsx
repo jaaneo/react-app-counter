dce0b42487d539dc759d77656988fc1723a4e234
Alejandro <jaaneo@gmail.com>
2023-03-02 19:55:58 -0300

    project finish

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,22 @@ function App() {
   }
   
   return (
-   <div>
-    <p>{count}</p>
-    <Button text="Aumentar" onClick={handleIncrement}/>
-    <Button text="Disminuir" onClick={handleDecrement}/>
-   </div>
+   
+
+    <div class="bg-gradient-to-r from-sky-500 to-indigo-500 p-6">
+
+            <h1 class="max-w-3xl mb-4 text-5xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">¡Challenge number two! </h1>
+            <h2 class="text-4xl font-bold dark:text-white">Current counter: {count}</h2>
+          
+           <p class="m-3">
+            <Button text="Disminuir" onClick={handleDecrement} />
+            <Button text="Aumentar" onClick={handleIncrement} />
+           </p>
+          
+    </div>
+
+
+  
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button ({text, onClick}){
     return(
-        <button onClick={onClick}>
-            {text}
+        <button onClick={onClick} class="text-gray-500 bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-green-500">
+           {text}
         </button>
     )
 }
